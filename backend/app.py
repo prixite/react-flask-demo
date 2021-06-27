107ab56ba18d43267ca591ec4b18aa9215628203
Umair Khan <umair.waheed@gmail.com>
2021-06-27 19:01:34 +0500

    Add endpoint for profile

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/profile")
+def profile():
+    return dict(
+        name='Umair Khan',
+        email='umair@example.com',
+        country='Pakistan',
+    )
