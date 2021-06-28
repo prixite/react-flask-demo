8beace6ace0bcddc113f602c852d1fd7e0e5747c
Umair Khan <umair.waheed@gmail.com>
2021-06-28 06:17:07 +0500

    Use generic data in backend

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ def hello_world():
 @app.route("/api/profile")
 def profile():
     return dict(
-        name='Umair Khan',
-        email='umair@example.com',
-        country='Pakistan',
+        name='John Doe',
+        email='john@example.com',
+        country='Temeria',
     )
